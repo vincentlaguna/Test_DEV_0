ee558c78b3dc096bcfcd14041cd2e42ce8afdcf8
vincentlaguna <vincentlaguna@live.com>
2021-08-15 22:13:11 +0000

    Creation of Feature branch to implement server accepting from multiple clients - commit #0

--- Inter-Process-Communication_Testing/CS_Build_v0/Srv_Build_v0/inc/CS_Common.h
+++ Inter-Process-Communication_Testing/CS_Build_v0/Srv_Build_v0/inc/CS_Common.h
@@ -1,46 +1,51 @@
 #pragma once
 /*****************************************************************************
-* Author(s):                                                                 *
-*                                                                            *
-* File: CS_Common.h                                                          *
-*                                                                            *
-* Description: Common header file for Client-Server code                     *
-*                                                                            *
+Author(s):                                                                 
+                                                                            
+File: CS_Common.h                                                          
+
+Description: Common header file for Client-Server code                     
+                                                                            
 *****************************************************************************/
 // Header Include Guards
 
-#pragma comment(lib, "ws2_32.lib")
+#pragma	  comment(lib, "ws2_32.lib")
 
-#ifndef		CS_COMMON_H
-#define 	CS_COMMON_H
+#ifndef	  CS_COMMON_H
+#define	  CS_COMMON_H
 
 #define	  LIN
 
 /* Includes: ****************************************************************/
 
-#ifndef		LIN
-#include	<winsock.h>
+#ifndef	  LIN
+#include  <winsock.h>
 #else
-#include	<sys/socket.h>
-#include	<arpa/inet.h>
-#include	<unistd.h>
+#include  <sys/socket.h>
+#include  <arpa/inet.h>
+#include  <unistd.h>
 #endif
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<stdint.h>
-#include	<string.h>
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <stdint.h>
+#include  <string.h>
 
 /****************************************************************************/
 
 /* Defines: *****************************************************************/
 
-// #define		LOCAL_TEST
-#define		TEST_PORT     				12345
-#define		TIME_V	   struct			timeval
-#define		S_SADDR	   struct			sockaddr
-#define		S_SADDR_IN struct			sockaddr_in
-#define		LOCAL_IP							"127.0.0.1"
-#define		MAX_STR_SZ						256
+// #define  LOCAL_TEST
+#define   TEST_PORT  12345
+#define	  IP0		 "192.168.135.21"
+#define	  IP1		 "192.168.135.22"
+#define	  IP2		 "192.168.135.23"
+#define	  IP3		 "192.168.135.24"
+#define	  IP4		 "192.168.135.25"
+#define   LOCAL_IP   "127.0.0.1"
+#define	  TIME_V     struct  timeval
+#define	  S_SADDR    struct	 sockaddr
+#define	  S_SADDR_IN struct	 sockaddr_in
+#define	  MAX_STR_SZ 256
 
 /****************************************************************************/
 
@@ -48,13 +53,12 @@
 
 /* Data Buffer **************************************************************/
 
-typedef	struct									DataBuffer
+typedef	struct  DataBuffer
 {
-	char													cPayload[MAX_STR_SZ]; // Storage for string	
-	// uint32_t										uBufferType;	      	// Type of Buffer	
-	uint32_t											uDataSize;	          // Size of data
-	uint16_t											*pNext;			      		// Next node *
-	uint16_t											*pPrev;			      		// Previous node *
+	char		cPayload[MAX_STR_SZ]; // Storage for string	
+	uint32_t	uDataSize;	          // Size of data
+	uint16_t	*pNext;	       		  // Next node *
+	uint16_t    *pPrev;			      // Previous node *
 
 } DataBuffer_t;
 
@@ -62,18 +66,18 @@ typedef	struct									DataBuffer
 
 /* Typedefs: ****************************************************************/
 
-// typedef		DataBuffer_t				*pDbuff;
+// typedef  DataBuffer_  *pDbuff;
 
 /****************************************************************************/
 
 /* Function Prototypes: *****************************************************/
 // Helper Functions
 
-uint16_t	SokInit_Handlr(void);
-uint32_t	BindSrvSok_Hndlr(uint32_t uSrvSok);
-uint32_t	SokConnect_Hndlr(uint32_t uClSok);
-uint32_t	SokSend_Hndlr(uint32_t uClSok, char *pRqst, uint16_t pRqstLen);
-uint32_t	SokRcv_Hndlr(uint32_t uClSok, char *pRsp, uint16_t rcvSize);
+uint16_t  SokInit_Handlr(void);
+uint32_t  BindSrvSok_Hndlr(uint32_t uSrvSok);
+uint32_t  SokConnect_Hndlr(uint32_t uClSok);
+uint32_t  SokSend_Hndlr(uint32_t uClSok, char *pRqst, uint16_t pRqstLen);
+uint32_t  SokRcv_Hndlr(uint32_t uClSok, char *pRsp, uint16_t rcvSize);
 
 /****************************************************************************/
 #endif // CS_COMMON_H
