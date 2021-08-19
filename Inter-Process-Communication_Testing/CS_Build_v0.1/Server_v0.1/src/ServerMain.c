525b3f0815245f323c6aac46256d0ec651e8d2db
vincentlaguna <vincentlaguna@live.com>
2021-08-19 18:10:33 +0000

    corrections to build and run

---
+++ Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/ServerMain.c
@@ -0,0 +1,56 @@
+/*****************************************************************************
+Author(s) or Contributor(s): Version 0.1  ~<NAME> 2021                                                                 
+
+File: ServerMain.c              
+
+Description: Server-side Main                                              
+
+*****************************************************************************/
+
+
+/* Includes: ****************************************************************/
+
+#include  "../inc/CS_Common.h"
+
+/****************************************************************************/
+
+/* Main Starts Here: ********************************************************/
+//
+int main(int argc, char *argv[])
+{
+  // Initialize Local Variables
+  uint16_t  uSrvSok = 0; 
+  uSrvSok = SokInit_Handlr();
+  // Winsock
+  #ifndef   LIN
+    
+    WSADATA wsaData;
+    
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+    {
+      fprintf(stderr,"WSAStartup() failed");
+      exit(1);
+    }
+    
+  #endif
+  // Error Handling
+  if (uSrvSok == -1)
+  {
+    printf("\nCreation of SOCKET Failed.\n");
+    return EXIT_FAILURE;
+  }
+  
+  SLEEP
+  
+  printf("\n>>> The SOCKET has been created >>>\n\n");
+
+  SrvConnection_Hndlr(uSrvSok, MAX_CONN);
+  
+  return(0);
+
+}
+
+/****************************************************************************/
+
+/****************************************************************************/
+// End ServerMain.c
