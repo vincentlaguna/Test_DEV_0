a3a974179c4e2593d8db9bc898c64516151199d3
vincentlaguna <vincentlaguna@live.com>
2021-08-30 17:43:28 +0000

    CS_Build_v0.1 - Finished basic checksum implementation on Client.c trying different sockotptions (KEEPALIVE w/ optVal = 5) - status = needs real-world testing! - commit #3

--- Inter-Process-Communication_Testing/CS_Build_v0.1/Client_v0.1/src/ClientMain.c
+++ Inter-Process-Communication_Testing/CS_Build_v0.1/Client_v0.1/src/ClientMain.c
@@ -45,8 +45,8 @@ int main(int argc, char *argv[])
   int optVal;
   socklen_t optLen = sizeof(optVal);
   uint8_t *IPbuffer;
-  uint8_t sndBuffer[MAX_LEN+1];
-  uint8_t rcvBuffer[MAX_LEN+1];
+  uint8_t sndBuffer[MAX_LEN];
+  uint8_t rcvBuffer[MAX_LEN];
   // For server data
   S_SADDR_IN  SrvAddr;
   // Time-out values for socket options
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
   #endif
  
   printf("\n[-]CLIENT-Side Socket Initialization = in progress...\n");
-  if ((connectSOKFD = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  if ((connectSOKFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) // TCP
   {
     printf("[-]Creation of SOCKET = FAIL\n");
     return EXIT_FAILURE;
@@ -125,7 +125,7 @@ int main(int argc, char *argv[])
   printf("[+]DATA sent to server = OK\n");
   // Zero-out buffer
   memset(rcvBuffer, '\0', MAX_LEN);
-  optVal = 5;
+  optVal = 1;
   // Output Server Response
   // if (setsockopt(connectSOKFD, SOL_SOCKET, SO_RCVTIMEO, (char *)&Tv, sizeof(Tv)) < 0)
   if (setsockopt(connectSOKFD, SOL_SOCKET, SO_KEEPALIVE, (char *)&optVal, optLen) < 0)
@@ -179,6 +179,152 @@ int main(int argc, char *argv[])
 }
 
 /****************************************************************************/
+
+
+/* Main Starts Here: ********************************************************
+//
+int main(int argc, char *argv[])
+{
+  // Initialize Local Variables
+  int connectSOKFD; 
+  int optVal;
+  socklen_t optLen = sizeof(optVal);
+  uint8_t *IPbuffer;
+  uint8_t sndBuffer[MAX_LEN];
+  uint8_t rcvBuffer[MAX_LEN];
+  // For server data
+  S_SADDR_IN  SrvAddr;
+  // Time-out values for socket options
+  // TIME_V       Tv;
+  // Tv.tv_sec  = TIME_O; // Time-Out in Seconds
+  // Tv.tv_usec = 0;
+  // Winsock
+  #ifndef LIN
+    
+    WSADATA wsaData;
+    
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+    {
+      fprintf(stderr,"[-]WSAStartup() = FAIL\n");
+      exit(1);
+    }
+    
+  #endif
+ 
+  printf("\n[-]CLIENT-Side Socket Initialization = in progress...\n");
+  if ((connectSOKFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) // TCP
+  {
+    printf("[-]Creation of SOCKET = FAIL\n");
+    return EXIT_FAILURE;
+  }
+  // SLEEP
+  printf("[+]CLIENT-Side Socket Initialization = OK\n");
+  // SLEEP
+  // Setup Server struct Info
+  memset(&SrvAddr, 0, sizeof(SrvAddr)); // Zero-out struct values
+  SrvAddr.sin_family = AF_INET;
+  SrvAddr.sin_port   = htons(REM_SRV_PORT);
+  // Get remote server address
+  if (inet_pton(AF_INET, REM_SRV_IP, &SrvAddr.sin_addr) <= 0)
+  {
+    printf("[-]ERROR on remote address: %s\n", REM_SRV_IP);
+    return EXIT_FAILURE;
+  }
+  // Connect to server
+  printf("[+]CONNECTION to Remote Server = in progress...\n");
+  // SLEEP
+  if (connect(connectSOKFD, (S_SADDR *)&SrvAddr, sizeof(SrvAddr)) < 0)
+  {
+    printf("[-]CONNECT to remote address: %s = FAIL\n", REM_SRV_IP);
+    return EXIT_FAILURE;
+  }
+  IPbuffer = inet_ntoa(SrvAddr.sin_addr);
+  
+  printf("[+]CONNECTION to Remote Server = OK\n");
+  // SLEEP
+  printf("[+]CONNECTED to remote address: %s\n", IPbuffer);
+  // Connected to server -> prepare the message to send
+  memset(sndBuffer, '\0', MAX_LEN);
+  strcpy(sndBuffer, cStringPayload);
+  // strcpy(sndBuffer, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"					    // 26
+  //                   "abcdefghijklmnopqrstuvwxyz"							// 52
+  //                   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"							// 78
+  //                   "abcdefghijklmnopqrstuvwxyz"							// 104
+  //                   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"							// 130
+  //                   "abcdefghijklmnopqrstuvwxyz"							// 156
+  //                   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"							// 182
+  //                 	"abcdefghijklmnopqrstuvwxyz"							// 208
+  //                   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"							// 234
+  //                   // "abcdefghijklmnopqrstuvwxyz"							// 260
+  //             	    "abcdefghijklmnopqrst"
+  //             	    "\n");
+  // Send data to the Remote Server
+  // SLEEP
+  printf("[-]SENDING data in send sndBuffer to server...\n");
+  // if (write(connectSOKFD, sndLine, sndBytes) != sndBytes)
+  if (write(connectSOKFD, sndBuffer, strlen(sndBuffer)) != strlen(sndBuffer))
+  {
+    printf("[-]WRITE ERROR on socket file descriptor SEND = FAIL\n");
+    return EXIT_FAILURE;
+  }
+  printf("[+]DATA sent to server = OK\n");
+  // Zero-out buffer
+  memset(rcvBuffer, '\0', MAX_LEN);
+  optVal = 1;
+  // Output Server Response
+  // if (setsockopt(connectSOKFD, SOL_SOCKET, SO_RCVTIMEO, (char *)&Tv, sizeof(Tv)) < 0)
+  if (setsockopt(connectSOKFD, SOL_SOCKET, SO_KEEPALIVE, (char *)&optVal, optLen) < 0)
+  {
+    printf("[-]TIME OUT ERROR\n");
+    return EXIT_FAILURE;
+  }
+  // SLEEP
+  printf("[-]SERVER = RECEIVING DATA... %s\n", rcvBuffer);
+  read(connectSOKFD, rcvBuffer, MAX_LEN-1);
+  // SLEEP
+  
+  while (rcvBuffer)
+  {
+    fprintf(stdout, "\n%s\n\n%s", convertHex(rcvBuffer, strlen(rcvBuffer)), rcvBuffer);
+    // Look for end of message
+    if (rcvBuffer[strlen(rcvBuffer)-1] == ('\n') || ('\0'))
+    break;
+  }
+  
+  // SLEEP
+  printf("\n[+]SERVER RESPONSE: %s\n", rcvBuffer);
+  // SLEEP
+  printf("[+]DATA RECEIVED = OK\n");
+  printf("[+]BYTES RECEIVED = %d\n", sizeof(rcvBuffer));
+  printf("[+]LENGTH RECEIVED = %d\n", strlen(rcvBuffer));
+  
+  if (bCheckSum(sndBuffer, rcvBuffer, sizeof(rcvBuffer)))
+  {
+    printf("[+]CHECKSUM = PASS\n");
+  }
+  else
+  {
+    printf("[+]CHECKSUM = FAIL\n");
+  }
+  putchar('\n');
+  // Zero-out receive buffer
+  memset(rcvBuffer, '\0', MAX_LEN);
+  // printf("[+]SIZE OF CHAR = %d, INT = %d, UINT8_T = %d\n\n", 
+        // sizeof(char), sizeof(int), sizeof(uint8_t)); // For debug
+  
+  #ifndef LIN
+    closesocket(connectSOKFD);
+    WSACleanup();
+  #else
+    close(connectSOKFD);
+  #endif
+  
+  return(0);
+
+}
+
+/****************************************************************************/
+
 
 /****************************************************************************/
 // End ClientMain.c
--- Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/ServerMain.c
+++ Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/ServerMain.c
@@ -22,9 +22,10 @@ int main(int argc, char *argv[])
   int listenSOKFD, connectSOKFD; 
   int sVal;
   S_SADDR_IN  SrvAddr; 
+  S_SADDR_IN  ClAddr;
   
-  uint8_t rcvBuffer[MAX_LEN+1];
-  uint8_t rplyBuffer[MAX_LEN+1];
+  uint8_t rcvBuffer[MAX_LEN];
+  uint8_t rplyBuffer[MAX_LEN];
   // Winsock
   #ifndef   LIN
     
@@ -40,7 +41,7 @@ int main(int argc, char *argv[])
   // Create Socket File Descriptor to listen on (Server)
   // SLEEP
   printf("\n[-]SERVER-Side Socket Initialization = in progress...\n");
-  if ((listenSOKFD = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  if ((listenSOKFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) // TCP
   {
     printf("[-]Creation of SOCKET = FAIL\n");
     return EXIT_FAILURE;
@@ -50,6 +51,7 @@ int main(int argc, char *argv[])
   // SLEEP
   // Setup Server Info
   memset(&SrvAddr, 0, sizeof(SrvAddr)); // Zero-out struct values
+  memset(&ClAddr, 0, sizeof(ClAddr)); // Zero-out struct values
   SrvAddr.sin_family      = AF_INET;
   SrvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
   SrvAddr.sin_port        = htons(REM_SRV_PORT);
@@ -80,7 +82,7 @@ int main(int argc, char *argv[])
   while(1)
   {
     // Initialize struct and variable for client info
-    S_SADDR_IN  ClAddr;
+    // S_SADDR_IN  ClAddr;
     socklen_t   clAddrLen;
     // Accept will block until incoming connection arrives,
     // returning Socket File Descriptor to the connection
@@ -126,6 +128,123 @@ int main(int argc, char *argv[])
 }
 
 /****************************************************************************/
+
+
+/* Main Starts Here: ********************************************************
+//
+int main(int argc, char *argv[])
+{
+  // Initialize Local Variables
+  int listenSOKFD, connectSOKFD; 
+  int sVal;
+  S_SADDR_IN  SrvAddr; 
+  S_SADDR_IN  ClAddr;
+  
+  uint8_t rcvBuffer[MAX_LEN];
+  uint8_t rplyBuffer[MAX_LEN];
+  // Winsock
+  #ifndef   LIN
+    
+    WSADATA wsaData;
+    
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+    {
+      fprintf(stderr,"[-]WSAStartup() = FAIL\n");
+      exit(1);
+    }
+    
+  #endif
+  // Create Socket File Descriptor to listen on (Server)
+  // SLEEP
+  printf("\n[-]SERVER-Side Socket Initialization = in progress...\n");
+  // if ((listenSOKFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) // TCP
+  if ((listenSOKFD = socket(AF_INET, SOCK_DGRAM, 0)) < 0) // UDP
+  {
+    printf("[-]Creation of SOCKET = FAIL\n");
+    return EXIT_FAILURE;
+  }
+  // SLEEP
+  printf("[+]SERVER-Side Socket Initialization = OK\n");
+  // SLEEP
+  // Setup Server Info
+  memset(&SrvAddr, 0, sizeof(SrvAddr)); // Zero-out struct values
+  SrvAddr.sin_family      = AF_INET;
+  SrvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  SrvAddr.sin_port        = htons(REM_SRV_PORT);
+  // Bind Call
+  // SLEEP
+  printf("[+]Binding to PORT: %d...\n", REM_SRV_PORT);
+  if ((bind(listenSOKFD, (S_SADDR *)&SrvAddr, sizeof(SrvAddr))) < 0)
+  {
+    perror("[-]BIND = FAIL\n"); // Print the error message
+  }
+  // SLEEP
+  printf("[+]Bind = OK\n");
+  // Listen Call
+  // SLEEP
+  if ((listen(listenSOKFD, MAX_CONN)) < 0)
+  {
+    printf("[-]LISTEN = FAIL\n");
+  }
+  printf("[+]# MAX CONNECTIONS = %d\n", MAX_CONN);
+  // SLEEP
+  printf("[+]LISTEN = OK\n");
+  // SLEEP
+  printf("[+]LISTENING ON PORT = %d\n", REM_SRV_PORT);
+  // SLEEP
+  printf("[-]Waiting for incoming connections...\n\n");
+  // SLEEP
+  // While-loop for server to listen on binded port for incoming connections
+  while(1)
+  {
+    // Initialize struct and variable for client info
+    // S_SADDR_IN  ClAddr;
+    socklen_t   clAddrLen;
+    // Accept will block until incoming connection arrives,
+    // returning Socket File Descriptor to the connection
+    // fflush(stdout);
+    // Accept Call
+    connectSOKFD = accept(listenSOKFD, (S_SADDR *)&ClAddr, (socklen_t *)&ClAddr);
+    if (connectSOKFD < 0)
+    {
+      printf("[-]INCOMING CONNECTION ACCEPT = FAIL\n");
+    }
+    printf("[+]INCOMING CONNECTION ACCEPT = OK\n");
+    // SLEEP
+    // Zero-out buffers
+    memset(rcvBuffer, '\0', MAX_LEN);
+    memset(rplyBuffer, '\0', MAX_LEN);
+    // Reading the client's message
+    printf("[-]SERVER = RECEIVING DATA...\n\n");
+    printf("[+]DATA RECEIVED: \n");
+    
+    while ((sVal = read(connectSOKFD, rcvBuffer, MAX_LEN-1)) > 0)
+    {
+      fprintf(stdout, "%s\n\n%s\n", convertHex(rcvBuffer, sVal), rcvBuffer);
+      // Look for end of message
+      if (rcvBuffer[strlen(rcvBuffer)-1] == ('\n') || ('\0'))
+        break;
+    }
+    printf("\nConfirm receive buffer: %s\n", rcvBuffer);
+    printf("[+]DATA RECIEVED = OK\n");
+    printf("[+]BYTES RECIEVED = %d\n", sizeof(rcvBuffer));
+    printf("[+]LENGTH RECEIVED = %d\n", strnlen(rcvBuffer, MAX_LEN));
+    strcpy(rplyBuffer, rcvBuffer);
+    printf("[-]SERVER = Replied data back to client...\n\n");
+    printf("[-]Waiting for incoming connections...\n\n");
+    write(connectSOKFD, rplyBuffer, strlen(rplyBuffer));
+    close(connectSOKFD);
+    // Zero-out buffers
+    memset(rcvBuffer, '\0', MAX_LEN);
+    memset(rplyBuffer, '\0', MAX_LEN);
+  }
+  
+  return(0);
+
+}
+
+/****************************************************************************/
+
 
 /****************************************************************************/
 // End ServerMain.c
