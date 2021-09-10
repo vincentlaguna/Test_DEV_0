4d4fbc9522532c1cb5a3c0f05ddc8de18aa9684b
vincentlaguna <vincentlaguna@live.com>
2021-09-10 17:29:21 +0000

    Starting to abstract a UDP-version of the server connection handler function - commit #1

--- Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/Server.c
+++ Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/Server.c
@@ -172,85 +172,45 @@ void  SrvConnection_Hndlr(uint32_t uSrvSok, uint16_t nConnections)
 
 Name:	UDP_SrvConnection_Hndlr()                                            
 Purpose: Handles incoming connections to the server                 
-Parameters: signed 32-bit integer                                          
+Parameters: unsigned uint8_t pointer                                          
 Returns: void                                        
 
 *****************************************************************************/
 
-void  UDP_SrvConnection_Hndlr(int32_t clSOKFD)
+void  UDP_SrvConnection_Hndlr(const uint8_t *remIP)
 {
-  // Local Variables
   // Receive and Reply Buffers
   uint8_t rcvBuffer[MAX_LEN];
   uint8_t rplyBuffer[MAX_LEN];
-  
-  uint16_t  sok   = 0; 
-  uint16_t  clLen = 0;
-  
-  S_SADDR   cL;
-  // Initialize buffers to store the data
-  DBffr  *SrvDbuff;
-  SrvDbuff = (DBffr *)malloc(sizeof(DBffr));
-  clLen = sizeof(S_SADDR_IN);
-  // Create Socket
-  uSrvSok = SokInit_Handlr();
-  // Bind
-  if (BindSrvSok_Hndlr(uSrvSok) < 0)
-  {
-    perror("BIND Failed."); // Print the error message
-  }
-  
-  SLEEP
-  
-  printf("\n<<< BIND Done >>>\n\n");
-  // Listen
-  listen(uSrvSok, nConnections); // Number of MAX connections
-  
-  printf("\n>>> Waiting for incoming connections...\n\n");
-  
+  // Local Variables
+  uint8_t remPort;
+  int listenSOKFD, clAddrLen;
+  // Local Structs
+  S_SADDR_IN SrvAddr, ClAddr;
+  // Assign remPort Port corresponding port number
+  // switch()
+  // Zero-out struct
+  memset(&SrvAddr, 0, sizeof(SrvAddr));
+  // Create a UDP Socket
+  listenSOKFD = socket(AF_INET, SOCK_DGRAM, 0);        
+  SrvAddr.sin_addr.s_addr = htonl(remIP);
+  SrvAddr.sin_port = htons(REM_SRV_PORT_0);
+  SrvAddr.sin_family = AF_INET; 
+  // bind server address to socket descriptor
+  bind(listenSOKFD, (struct sockaddr*)&SrvAddr, sizeof(SrvAddr));
+  //Receive the datagram
+  clAddrLen = sizeof(ClAddr);
   while (1)
   {
-    // Accept connection from an incoming client
-    sok = accept(uSrvSok, (S_SADDR *)&cL, (socklen_t *)&cL);
-    
-    if (sok < 0)
-    {
-      perror("ACCEPT Failed.");
-    }
-        
-    printf("\nConnection ACCEPTED\n\n");
-    // Buffers
-    uint32_t  DbuffSize = sizeof(DBffr);
-    memset(&SrvDbuff->cPayload, '\0', MAX_STR_SZ);
-    // Receive a reply from the Client
-    if (recv(sok, &SrvDbuff->cPayload, (uint32_t)DbuffSize, 0) < 0)
-    {
-      printf("\nRECEIVE Failed.\n");
-    }
-      
-    printf("Client Message: %s\n", SrvDbuff->cPayload);
-    // Send some data
-    if(send(sok, &SrvDbuff->cPayload, MAX_STR_SZ, 0) < 0)
-    {
-      printf("\nSEND Failed.\n");
-    }
-      
-    printf("\n<<< Waiting for incoming connections...\n");
-    // // Accept Connection from another incoming Client
-    // sok = accept(uSrvSok, (S_SADDR *)&cL, (socklen_t*)&clLen);
-     
-    // if (sok < 0)
-    // {
-    //   perror("ACCEPT Failed.");
-    // }
-    // printf("\nConnection ACCEPTED\n\n");
-      
-    // #ifndef LIN
-    //   closesocket(uSrvSok);
-    //   WSACleanup();
-    // #else
-    //   close(uSrvSok);
-    // #endif
+    // receive message
+    int n = recvfrom(listenSOKFD, rcvBuffer, sizeof(rcvBuffer),
+                     0, (struct sockaddr*)&ClAddr,&clAddrLen);
+    rcvBuffer[n] = '\0';
+    puts(rcvBuffer);
+    strcpy(rplyBuffer, rcvBuffer);         
+    // send the response
+    sendto(listenSOKFD, rplyBuffer, MAX_LEN, 0,
+          (struct sockaddr*)&ClAddr, sizeof(ClAddr));
   }
   
   SLEEP
@@ -301,3 +261,8 @@ char  *convertHex(const char *src, size_t len)
 
 /****************************************************************************/
 // End Server.c
+
+void  UDP_SrvConnection_Hndlr(const uint8_t *remIP)
+{
+  printf("\n%s\n\n", remIP);
+}
