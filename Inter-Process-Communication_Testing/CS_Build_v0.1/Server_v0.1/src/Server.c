05273fa4b5369bb4ee857f4fb58b27faf733937a
vincentlaguna <vincentlaguna@live.com>
2021-09-20 13:32:22 +0000

    Multiple UDP Server Instances (5) Configured to bind to unique IP/PORTS - Ready For TESTING - Working (local modifications made for local testing on Server_v0.1) commit #2

--- Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/Server.c
+++ Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/Server.c
@@ -251,7 +251,7 @@ Purpose: Handles incoming connections to the server
 Parameters: unsigned uint8_t pointer                                          
 Returns: void                                        
 
-*****************************************************************************
+*****************************************************************************/
 
 void  UDP_SrvConnection_Hndlr(const uint16_t clSOKFD)
 {
