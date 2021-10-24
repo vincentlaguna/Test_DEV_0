04ce178541e95141fd01b3cb9d1225536af839bf
vincentlaguna <vincentlaguna@live.com>
2021-10-24 17:23:56 +0000

    - CS_Build_v0.1 (master) -> [Disregarding Server/Client_v0.1 - back to basics... This is the point where I took out the ipData struct assignment (in ServerMain.c) and it doesn't seg-fault...] - Server_vETHx instances are RPi4 RELEASE-READY - Commit #5 - 102421

--- Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/ServerMain.c
+++ Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/ServerMain.c
@@ -268,8 +268,10 @@ int main(int argc, char *argv[])
       sokData0.ipData->srvAddr.sin_addr.s_addr =
       *(uint8_t *)malloc(sizeof(uint8_t) * IP_STR_SZ);
       
-      // sokData0.ipData->srvAddr.sin_addr.s_addr =
-      // inet_addr(szRem_Srv_IP[eREM_SRV_IP_0]);
+      // This portion right here is causing the seg-fault...
+      sokData0.ipData->srvAddr.sin_addr.s_addr =
+      inet_addr(szRem_Srv_IP[eREM_SRV_IP_0]);
+      // End of first seg-fault section
       
       // sokData0.ipData->srvAddr.sin_port = htons(REM_SRV_PORT_0);
       // sokData0.ipData->clAddr = {0};
