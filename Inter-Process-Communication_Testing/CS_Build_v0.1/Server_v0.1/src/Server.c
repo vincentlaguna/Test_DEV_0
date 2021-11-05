381c05370fe721d54eb4996343dd4c17825119be
vincentlaguna <vincentlaguna@live.com>
2021-11-05 17:54:50 +0000

    - CS_Build_v0.1 (master) -> [Disregarding Server/Client_v0.1 - back to basics... non-threaded, S_SADDR_IN structs are local for now, trying to assign in Server.c before venturing back into the weeds doing multi-threading... Still got nowhere...] - (Cleanup and IP change, please test) Server_vETHx instances are RPi4 RELEASE-READY - NEEDS TESTING - Commit #25 - 110521

--- Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/Server.c
+++ Inter-Process-Communication_Testing/CS_Build_v0.1/Server_v0.1/src/Server.c
@@ -256,7 +256,7 @@ Purpose: Handles incoming connections to the server
 Parameters: unsigned uint8_t pointer                                          
 Returns: void                                        
 
-*****************************************************************************
+*****************************************************************************/
 
 
 // void  UDP_SrvConnection_Hndlr(const uint16_t clSOKFD)
