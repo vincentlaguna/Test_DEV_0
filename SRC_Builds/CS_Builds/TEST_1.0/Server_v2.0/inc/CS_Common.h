7fd1aa839ad0c05d55d0f70595a78b3c0548c1dc
vincentlaguna <vincentlaguna@live.com>
2021-11-22 17:12:01 +0000

    TEST not ready - commit #2 - [ PROD RELEASE = RELT_1.0 ] - 11222021

--- SRC_Builds/CS_Builds/TEST_1.0/Server_v2.0/inc/CS_Common.h
+++ SRC_Builds/CS_Builds/TEST_1.0/Server_v2.0/inc/CS_Common.h
@@ -1,10 +1,10 @@
 #pragma once
 /*****************************************************************************
-Author(s) or Contributor(s): Version 0.1  ~Vincent A. Laguna 2021                                                               
+Author(s) or Contributor(s): Version 2.0 (TEST)  ~Vincent A. Laguna 2021                                                               
   
 File: CS_Common.h                                                          
 
-Description: Common header file for Client-Server code                     
+Description: Common Header File for Mutli-Threaded Server code                     
 
 *****************************************************************************/
 // Header Include Guards
@@ -19,13 +19,18 @@ Description: Common header file for Client-Server code
 /* Includes: ****************************************************************/
 
 #ifndef	  LIN
-#include  <winsock.h>
-#include  <Windows.h>
+  
+  #include  <winsock.h>
+  #include  <Windows.h>
+
 #else
-#include  <sys/socket.h>
-#include  <arpa/inet.h>
-#include  <unistd.h>
+
+  #include  <sys/socket.h>
+  #include  <arpa/inet.h>
+  #include  <unistd.h>
+
 #endif
+
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <time.h>
@@ -152,59 +157,27 @@ static const uint8_t *cStringPayload =
 
 /* Data Buffer **************************************************************/
 
-typedef struct  IPData
-{
-  // IP Data Struct
-  S_SADDR_IN 	srvAddr;
-  S_SADDR_IN	clAddr;
-  uint8_t			IPDataID;
-		
-} IPData;
-
 typedef struct  SOKData
 {
   // Data to pass as argument to thread handler
   uint8_t   			*cIP;	  //[IP_STR_SZ];	// IP (c-String)
 	uint16_t				uPort;	// Port Number
 	uint16_t 				SOKid;	// SOK ID
-	struct IPData	  *ipData;
 	
 } SOKData;
-
-// struct  DataBuffer
-// {
-//   char  cPayload[MAX_STR_SZ]; // Storage for string	
-//   // uint32_t	uDataSize;	   // Size of data
-//   // uint16_t	*pNext;	       // Next node *
-//   // uint16_t  *pPrev;	       // Previous node *
-
-// } __attribute__((__packed__));
 
 /****************************************************************************/
 
 /* Typedefs: ****************************************************************
-
-// typedef  DataBuffer_  *pDbuff;
-typedef	struct DataBuffer DBffr;
-
 /****************************************************************************/
 
 /* Function Prototypes: *****************************************************/
 // Helper Functions
-int 			rID_Gen(void);
-uint8_t	  *convertHex(uint8_t *src, size_t len);
-bool      bCheckSum(const uint8_t *buff1, const uint8_t *buff2, size_t sZ);
-// void      UDP_SrvConnection_Hndlr(const uint16_t clSOKFD);
-// void      *UDP_SrvConnection_Hndlr(void *clSOKFD);
-// void      UDP_SrvConnection_Hndlr(void *SOKData);
 void      *UDP_SrvConnection_Hndlr(void *SOKData);
 int16_t   UDP_SokInit_Handlr(void);
-int32_t   BindSrvSok_Hndlr(int16_t SrvSok, const uint8_t *szRemIP);
-// void	    SrvConnection_Hndlr(uint32_t uSrvSok, uint16_t nConnections);
-// int16_t  SokInit_Handlr(void);
-// uint32_t  SokConnect_Hndlr(uint32_t uClSok, char* remIP, uint16_t remPort);
-// uint32_t  SokSend_Hndlr(uint32_t uClSok, char *pRqst, uint16_t pRqstLen);
-// uint32_t  SokRcv_Hndlr(uint32_t uClSok, char *pRsp, uint16_t rcvSize);
+uint8_t	  *convertHex(uint8_t *src, size_t len);
+bool      bCheckSum(const uint8_t *buff1, const uint8_t *buff2, size_t sZ);
+int 			rID_Gen(void);
 
 /****************************************************************************/
 #endif // CS_COMMON_H
