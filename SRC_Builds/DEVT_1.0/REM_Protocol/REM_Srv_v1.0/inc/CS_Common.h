6978fc165df86239497fa4c607408f67b6ba1b44
vincentlaguna <vincentlaguna@live.com>
2022-05-06 21:05:16 -0400

    test command over Remote protocol - Commit #1 - 05062022

--- SRC_Builds/DEVT_1.0/REM_Protocol/REM_Srv_v1.0/inc/CS_Common.h
+++ SRC_Builds/DEVT_1.0/REM_Protocol/REM_Srv_v1.0/inc/CS_Common.h
@@ -64,13 +64,15 @@ Description: Common header file for REMOTE Server code
 #define NUM_SRV_THREADS 1
 #define NUM_CL_THREADS 1
 #define REM_CL_PORT 11069
-#define REM_SRV_PORT_0 11000
+#define REM_SRV_PORT_0 2077 // Remote Protocol Test Port
+// #define REM_SRV_PORT_0 11000
 #define REM_SRV_PORT_1 11001
 #define REM_SRV_PORT_2 11002
 #define REM_SRV_PORT_3 11003
 #define REM_SRV_PORT_4 11004
 #define REM_SRV_PORT_5 11005
-#define REM_SRV_IP_0 "127.0.0.1" // For quick local test
+#define REM_SRV_IP_0 "192.168.66.52" // Remote Device IP
+// #define REM_SRV_IP_0 "127.0.0.1" // For quick local test
 // #define	  REM_SRV_IP_0 "192.168.143.20"
 // #define	  REM_SRV_IP_0 "192.168.128.250"
 // #define REM_SRV_IP_1 "127.0.0.1" // Local Tests
@@ -196,18 +198,31 @@ static const uint8_t *cStringPayload =
 //   "",
 // };
 
+// static const uint8_t *cREMDataRFOn =
+// {
+//   // "\xff" 
+//   // "\x02"  
+//   "\x7a\x7f\x9c\x6e\xa3\x5f\x9c\xeb\xe8\xf4\xed\xa6\x08\x00\x45\x00\x00"
+//   "\x55\x40\xfa\x00\x00\x80\x11\x00\x00\xc0\xa8\x42\x45\xc0\xa8\x42\x34"
+//   "\xc5\x15\x08\x1d\x00\x41\x06\x1d\x0a\x0b\x20\x11\x52\x32\xff\xff\x00"
+//   "\x00\x0a\x00\x00\x00\xff\x60\x01\x00\x00\x00\xff\xff\xff\xff\x00\x00"
+//   "\x00\x00\x00\x00\x00\x00\x10\x00\x00\x00\xff\xff\xff\xff\x00\x00\x00"
+//   "\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0\xff\xff\xff\x27"
+//   // "\x03",
+//   // "",
+// };
+
 static const uint8_t *cREMDataRFOn =
 {
-  // "\xff" 
-  // "\x02"  
-  "b6c565d971719cebe8f4eda60800450000"
-  "55bf52000080110000c0a84245c0a84234"
-  "feef081d0041061d0a0b20115232ffff00"
-  "000a000000ff6000000000ffffffff0000"
-  "00000000000010000000ffffffff000000"
-  "000000000000000000f0ffffff26"
-  // "\x03",
-  "",
+  "\xff"                                               // NUM bytes
+  "\x02"                                               // STX
+  "01234567890123456789012345678901234567890123456789" // 50 chrs
+  "01234567890123456789012345678901234567890123456789" // 50 chrs
+  "01234567890123456789012345678901234567890123456789" // 50 chrs
+  "01234567890123456789012345678901234567890123456789" // 50 chrs
+  "01234567890123456789012345678901234567890123456789" // 50 chrs
+  "\x03",                                              // ETX
+                                                       // 255 total
 };
 
 /****************************************************************************/
