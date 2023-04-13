1a03d04206dfb421899106a971837fde9b46945f
vincentlaguna <vincentlaguna@live.com>
2023-04-13 15:21:23 -0700

    need to write an MIB module... 041323

--- SRC_Builds/TEST_1.0/snmp_demo/snmpdemoapp.c
+++ SRC_Builds/TEST_1.0/snmp_demo/snmpdemoapp.c
@@ -5,7 +5,7 @@
 //#define   OTHER_METHODS
 
 /* change the word "define" to "undef" to try the (insecure) SNMPv1 version */
- #undef   DEMO_USE_SNMP_VERSION_3
+ #define   DEMO_USE_SNMP_VERSION_3
  
  #ifdef   DEMO_USE_SNMP_VERSION_3
   #include  "net-snmp/transform_oids.h"
@@ -69,7 +69,7 @@
     session.version = SNMP_VERSION_1;
 
     // Set the SNMPv1 community name used for authentication
-    session.community     = "demopublic";
+    session.community     = "public";
     session.community_len = strlen(session.community);
    
   #endif // SNMPv1 
@@ -100,7 +100,7 @@
 
   // Create the PDU for the data (for our request).
   // 1) We're going to GET the system.sysDescr.0.node.
-  pdu = snmp_pdu_create(SNMP_MSG_GETNEXT);
+  pdu = snmp_pdu_create(SNMP_MSG_GET);
   anOID_len = MAX_OID_LEN;
   // So, let's fill it with our requested oid. 
   // Let's get the system.sysDescr.0 variable for this example. 
@@ -109,16 +109,16 @@
   // into the anOID array we created above, or you could use one of 
   // the following function calls to do it. We recommend the first one (get_node), 
   // as it is the most powerful and accepts more types of OIDs. 
-  if (!snmp_parse_oid(".1.3.6.1.2.1.1.1.0", anOID, &anOID_len)) 
+  if (!snmp_parse_oid(".1.3.6.1.4.1.2021.13.4242.1.1.2", anOID, &anOID_len)) 
   {
-    snmp_perror(".1.3.6.1.2.1.1.1.0");
+    snmp_perror(".1.3.6.1.4.1.2021.13.4242.1.1.2");
     SOCK_CLEANUP;
     exit(1);
   }
 
-  get_node("sysDescr.0", anOID, &anOID_len);
+  get_node(".1.3.6.1.4.1.2021.13.4242.1.1.2", anOID, &anOID_len);
   //read_objid(".1.3.6.1.2.1.1.1.0", anOID, &anOID_len); // <- This method times out 040523
-  read_objid("system.sysDescr.0", anOID, &anOID_len); // <- Timeout (Sub-id not found: (top) -> system)
+  read_objid(".1.3.6.1.4.1.2021.13.4242.1.1.2", anOID, &anOID_len); // <- Timeout (Sub-id not found: (top) -> system)
   
   #if OTHER_METHODS
     get_node("sysDescr.0", anOID, &anOID_len);
